ce460c8104b8633825ca05f6e2bc654840c6981a
BlackNordicDude <48724415+BlackNordicDude@users.noreply.github.com>
2022-05-27 02:27:20 +0300

    add active tab

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useRef } from "react";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import IngredientCategory from '../ingredients-list/ingredients-list';
 import style from '../burger-ingredients/burger-ingredients.module.css';
@@ -15,16 +15,43 @@ const BurgerIngredients = () => {
     })
     const dispatch = useDispatch();
 
-    const onTabClick = (tab) => {
-        dispatch({type: CHANGE_TAB, tab: tab})                           
-        const elem = document.getElementById(currentTab);  
-        if (elem) elem.scrollIntoView({ behavior: "smooth" });
-    }
+    const scrollRef = useRef(null);
+    const bunRef = useRef(null);
+    const sauceRef = useRef(null);
+    const mainRef = useRef(null);                          
 
     const buns = useMemo(() => ingredients.filter(el => el.type === 'bun'),[ingredients]) 
     const sauces = useMemo(() => ingredients.filter(el => el.type === 'sauce'),[ingredients]) 
     const main = useMemo(() => ingredients.filter(el => el.type === 'main'),[ingredients]) 
     
+    const onTabClick = (tab) => {
+        dispatch({type: CHANGE_TAB, tab: tab}) 
+    }
+
+    const handleScroll = () => {
+        const scrollBlockPos = scrollRef.current.getBoundingClientRect().top;
+
+        const bunHeaderPos = bunRef.current.getBoundingClientRect().top;
+        const sauceHeaderPos = sauceRef.current.getBoundingClientRect().top;
+        const mainHeaderPos = mainRef.current.getBoundingClientRect().top;
+
+        const bunsDif = Math.abs(scrollBlockPos - bunHeaderPos);
+        const saucesDif = Math.abs(scrollBlockPos - sauceHeaderPos);
+        const mainsDif = Math.abs(scrollBlockPos - mainHeaderPos);
+
+        console.log(scrollBlockPos, 'scrollBlock');
+        console.log(bunHeaderPos, 'bunHeader');
+        console.log(sauceHeaderPos, 'sauceHeaderPos');
+        console.log(mainHeaderPos, 'mainHeaderPos');
+
+        if (bunsDif < saucesDif) {
+            onTabClick("buns") 
+        } else if (saucesDif < mainsDif) {
+            onTabClick("sauces")
+        } else {
+            onTabClick("main")
+        }
+    }
 
     return (
         <section className="mr-10">
@@ -39,10 +66,10 @@ const BurgerIngredients = () => {
                     Начинка
                 </Tab>
             </ul>
-            <ul className={style.ingredients}>
-                <IngredientCategory title='Булки' id='buns' categoryData={buns}/>
-                <IngredientCategory title='Соусы' id='sauces' categoryData={sauces}/>
-                <IngredientCategory title='Начинка' id='main' categoryData={main}/>
+            <ul className={style.ingredients} ref={scrollRef} onScroll={handleScroll}>
+                <IngredientCategory title='Булки' id='buns' categoryData={buns} refer={bunRef}/>
+                <IngredientCategory title='Соусы' id='sauces' categoryData={sauces} refer={sauceRef}/>
+                <IngredientCategory title='Начинка' id='main' categoryData={main} refer={mainRef}/>
             </ul>
         </section>
     )
